543a64e7bf074bf78fedfeb9120c3b20c260384d
Ntrancha <101010.nk@gmail.com>
2015-04-16 10:02:38 +0200

    appli

---
+++ appli/script.js
@@ -0,0 +1,16 @@
+ function MonPrototype() {
+   this.a = 1;
+   this.b = function() {
+     return 'prototype';
+   };
+   this.e = 3;
+ }
+ function MaClasse() {
+   this.c = 2;
+   this.d = function() {
+     return 'classe';
+   };
+ 
+   this.e = 4;
+ }
+ 
