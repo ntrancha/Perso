4cd8271a271ee643173884d11bf4e307072fd380
Ntrancha <101010.nk@gmail.com>
2015-05-02 13:22:29 +0200

    Interface en cours

--- bin/interface_nk.py
+++ bin/interface_nk.py
@@ -1,68 +1,56 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-#import global_nk
+import global_nk
 from Tkinter import *
 from tkMessageBox import *
 
 class Interface(Frame):
 
+    def infos(self):
+        self.zone_info.create_text(250, 20, text="Débug", font="Arial 16 italic", fill="red")
+        self.zone_info.create_text(50, 45, text="Status", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 70, text="Jaune", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 95, text="Jouer", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 120, text="Maps", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 145, text="Mort", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 170, text="Partie", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 195, text="Talent", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 220, text="Pv", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 245, text="Mana", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 270, text="Score", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(50, 295, text="Player", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 45, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 70, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 95, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 120, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 145, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 170, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 195, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 220, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 245, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 270, text="Na", font="Arial 16 italic", fill="blue")
+        self.zone_info.create_text(150, 295, text="Na", font="Arial 16 italic", fill="blue")
+        
+
     def canevas(self):
-        self.zone_dessin = Canvas(self, width=500, height=500, bg='yellow',bd=8,relief="ridge")
-        self.zone_dessin.create_text(75, 60, text="Cible", font="Arial 16 italic", fill="blue")
-        self.zone_dessin.create_text(50, 20, text="test", font="Arial 16 italic", fill="blue")
-        self.zone_dessin.pack(side="left")
+        self.zone_info = Canvas(self, width=500, height=500, bg='white',bd=8,relief="ridge")
+        self.infos()
+        self.zone_info.pack(side="left")
         self.zone_dessin2 = Canvas(self, width=500, height=500, bg='red',bd=8,relief="ridge")
         self.zone_dessin2.focus_set()
         self.zone_dessin2.bind("<Key>", self.clavier)
+        self.photo = PhotoImage(file ='../img/status/test.gif')
+        self.zone_dessin2.create_image(120, 90, image = self.photo)
         self.zone_dessin2.pack(side="right")
-
-    def frame(self, mode):
-        if mode == 0:
-            # frame 1
-            self.Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE, bg="red")
-            self.Frame1.pack(side=LEFT)
-
-            # frame 2
-            Frame2 = Frame(fenetre, borderwidth=2, relief=GROOVE)
-            Frame2.pack(side=LEFT, padx=10, pady=10)
-
-            # frame 3 dans frame 2
-            Frame3 = Frame(Frame2, bg="white", borderwidth=2, relief=GROOVE)
-            Frame3.pack(side=RIGHT, padx=5, pady=5)
-
-            # Ajout de labels
-            Label(self.Frame1, text="Frame 1").pack(padx=100, pady=100)
-            Label(Frame2, text="Frame 2").pack(padx=10, pady=10)
-            Label(Frame3, text="Frame 3",bg="white").pack(padx=10, pady=10)
-        if mode == 1:
-            print "toto"
 
     def __init__(self, fenetre, **kwargs):
         #Frame.__init__(self, fenetre, width=368, height=176, **kwargs)
         Frame.__init__(self, fenetre)
         self.pack()
-        self.nb_clic = 0
         fenetre.title("NK")
         self.menu_bar()
-        #self.frame(0)
         self.canevas()
-        
-    def test_interface(self):
-        # Création de nos widgets
-        
-        self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
-        self.bouton_quitter.pack(side="left")
-        
-        self.bouton_cliquer = Button(self, text="Cliquez ici", fg="red", command=self.stop)
-        self.bouton_cliquer.pack(side="right")
-        zone_dessin = Canvas(self, width=500, height=500, bg='yellow',bd=8,relief="ridge")
-        zone_dessin.pack()
-        zone_dessin.create_rectangle(100,100,200,200)
-
-        fenetre['bg']='white'
-
-
 
     def menu_bar(self):
         menubar = Menu(self)
@@ -88,8 +76,10 @@ class Interface(Frame):
 
     def start(self):
         self.menu1.entryconfig(0, label="Coucou")
-        self.zone_dessin.itemconfigure(1, text="toto")
-        self.zone_dessin.itemconfigure(2, text="OK")
+        self.zone_info.itemconfigure(13, text="toto")
+        self.zone_info.itemconfigure(14, text="OK")
+        #self.photo = PhotoImage(file ='test2.gif')
+        #self.zone_dessin2.create_image(120, 90, image = self.photo)
 
     def cliquer(self):
         self.nb_clic += 1
@@ -105,6 +95,8 @@ class Interface(Frame):
     def clavier(self, event):
         touche = event.keysym
         #self.frame(1)
+        if touche == 'q':
+            self.quit()
         print(touche)
 
 if __name__=='__main__':
